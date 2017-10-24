c99b5e094c3abd3a96af54ca4103ed13b5029f3e
Thomas Bowen <tbowen1@outlook.com>
2017-10-24 16:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
--
--function happyHolidays() {
--  return "Happy holidays!"
--}
+
+const expect = require('chai').expect
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+
+describe('functions', () => {
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+  })
+
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).to.equal('Happy holidays!')
+    })
+  })
+
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).to.equal('Happy holidays, you!')
+    })
+  })
+
+  describe('happyHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyHolidayTo('Independence Day', 'you')).to.equal('Happy Independence Day, you!')
+    })
+  })
+
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).to.equal("It's 20 days until Mother's Day!")
+    })
+  })
+})
